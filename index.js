a8493a7ac348a68720c6a12f2adc9d8ae7159216
Cuong Liam <cuongdeptrai2706@gmail.com>
2025-08-19 10:02:06 +0700

    up

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log('Happy developing ✨')
+let phoneBooks = [];
+
+function addContact(name, phone, email, arr){
+    const newUsers = {name, phone, email};
+    arr.push(newUsers);
+}
+
+function displayContact(arr){
+    if (arr.length === 0){
+        console.log("nothing in arr");
+        return;
+    }
+
+    arr.forEach(function (val, idx){
+        console.log(`${idx + 1}. name: ${val.name}
+   phone: ${val.phone}
+   email: ${val.email}`);
+    })
+}
+
+addContact("abc", "0844334", "@gmail.com", phoneBooks);
+addContact("asd", "0844334", "@gmail.com", phoneBooks);
+addContact("oqwie", "0844334", "@gmail.com", phoneBooks);
+displayContact(phoneBooks);
